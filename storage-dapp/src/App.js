4c6cfb3fb21297dde9228d76a59de5662fd741ff
thopatevijay <thopatevijay@gmail.com>
2022-12-22 12:34:34 +0530

    interact with contract = init web3 lib and call storedData

--- storage-dapp/src/App.js
+++ storage-dapp/src/App.js
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import './App.css';
+import Web3 from "web3";
+import Storage from "./ABI/Storage.json";
+
 
 function App() {
+  const [value, setValue] = useState("")
+  const ethereum = window.ethereum;
+  const web3 = new Web3(ethereum);
+
+  const contractAddress = Storage.networks["5777"].address;
+  const contractAbi = Storage.abi;
+
+  const myContract = new web3.eth.Contract(contractAbi, contractAddress);
+
+  const getData = async () => {
+    const data = await myContract.methods.storedData().call();
+    console.log(data)
+    setValue(data)
+  }
+
+  getData();
   return (
     <div className="App">
       <header className="App-header">
         <p>
           Storage dApp.
+        </p>
+
+        <p>
+          Stored Data : {value}
         </p>
       </header>
     </div>
